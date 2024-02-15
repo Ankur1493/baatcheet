54c40d2adbdfec99fdaef59f38b950c64f5c3ae6
Ankur1493 <ankursharma1493@gmail.com>
2024-02-15 16:28:45 +0530

    basic socket io added, also added cors to remove cors errors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-import express from "express";
-const port = 8000;
+
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
-app.listen(port, () => {
-  console.log("server running")
-})
+httpServer.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
+
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,10 +1,21 @@
-import Messages from "./components/Messages"
-import Input from "./components/Input"
+
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+import { io } from "socket.io-client";
+
 export default function App() {
+  const socket = io("http://localhost:3000", {
+    withCredentials: true,
+    extraHeaders: {
+      "my-custom-header": "abcd"
+    }
+  });
+
   return (
     <div className="">
       <Messages />
       <Input />
     </div>
-  )
+  );
 }
+
