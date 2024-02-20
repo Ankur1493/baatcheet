4b47bee9672c686bfcd74d112551f1f89ef2bc32
Ankur1493 <ankursharma1493@gmail.com>
2024-02-21 00:36:34 +0530

    css changes

--- web/src/components/Messages.jsx
+++ web/src/components/Messages.jsx
@@ -1,7 +1,9 @@
+import { useUser } from '@clerk/clerk-react';
 import React, { useState, useEffect } from 'react'
 
 const Messages = ({ socket }) => {
 
+  const user = useUser();
   const [messages, setMessages] = useState([]);
   useEffect(() => {
     socket.on("message", (message) => {
@@ -17,7 +19,11 @@ const Messages = ({ socket }) => {
       <ul>
         {
           messages.map((msg, index) => (
-            <li key={index}>{msg}</li>
+            <li className='py-1' key={index}>
+              <div className='flex mx-32  h-[32px] items-center bg-sky-950 rounded-[10px] text-white w-fit border-sky-50 border-[1px]'>
+                <div className='mx-6'>{msg}</div>
+              </div>
+            </li>
           ))
         }
       </ul>
